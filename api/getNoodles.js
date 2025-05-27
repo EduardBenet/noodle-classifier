25d4a41f2bc78178d380a715d14030a0d30aada9
Eduard Benet Cerdà <ebenetce@mathworks.com>
2025-05-27 22:29:02 +0100

    undo test

--- api/getNoodles.js
+++ api/getNoodles.js
@@ -1,6 +1,17 @@
+const { CosmosClient } = require("@azure/cosmos");
+
+const endpoint = process.env.COSMOS_DB_ENDPOINT;
+const key = process.env.COSMOS_DB_KEY;
+const client = new CosmosClient({ endpoint, key });
+
 module.exports = async function (context, req) {
+    const database = client.database('noodles');
+    const container = database.container('packages');
+
+    const { resources: items } = await container.items.readAll().fetchAll();
+
     context.res = {
-        status: 200,
-        body: [{ id: "test-1", name: "Shin Ramyun" }]
+        headers: { 'Content-Type': 'application/json' },
+        body: items
     };
 };
