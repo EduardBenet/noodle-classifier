7f31ae1effaf1ed46d123b8b41039b2e84483307
Eduard Benet Cerdà <ebenetce@mathworks.com>
2025-05-27 21:04:00 +0100

    added azure connection

--- api/getNoodles.js
+++ api/getNoodles.js
@@ -1,7 +1,17 @@
+const { CosmosClient } = require("@azure/cosmos");
+
+const endpoint = process.env.COSMOS_DB_ENDPOINT;
+const key = process.env.COSMOS_DB_KEY;
+const client = new CosmosClient({ endpoint, key });
+
 module.exports = async function (context, req) {
-    const data = require('../data/noodles.json');
+    const database = client.database('noodles');
+    const container = database.container('packages');
+
+    const { resources: items } = await container.items.readAll().fetchAll();
+
     context.res = {
-        headers: { "Content-Type": "application/json" },
-        body: data
+        headers: { 'Content-Type': 'application/json' },
+        body: items
     };
 };
