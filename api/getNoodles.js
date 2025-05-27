cf1807a83172fedbe47e5e7d80fafe3925cf7cd8
Eduard Benet Cerdà <ebenetce@mathworks.com>
2025-05-27 22:22:44 +0100

    added test function

--- api/getNoodles.js
+++ api/getNoodles.js
@@ -1,17 +1,6 @@
-const { CosmosClient } = require("@azure/cosmos");
-
-const endpoint = process.env.COSMOS_DB_ENDPOINT;
-const key = process.env.COSMOS_DB_KEY;
-const client = new CosmosClient({ endpoint, key });
-
 module.exports = async function (context, req) {
-    const database = client.database('noodles');
-    const container = database.container('packages');
-
-    const { resources: items } = await container.items.readAll().fetchAll();
-
     context.res = {
-        headers: { 'Content-Type': 'application/json' },
-        body: items
+        status: 200,
+        body: [{ id: "test-1", name: "Shin Ramyun" }]
     };
 };
