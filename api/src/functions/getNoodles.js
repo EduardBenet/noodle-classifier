cb71f18ac24ed431334ca5bf03d1397327820efe
Eduard Benet Cerdà <ebenetce@mathworks.com>
2025-05-28 02:44:14 +0100

    test client

--- api/src/functions/getNoodles.js
+++ api/src/functions/getNoodles.js
@@ -4,17 +4,29 @@ const { CosmosClient } = require('@azure/cosmos');
 const endpoint = process.env.COSMOS_DB_ENDPOINT;
 const key = process.env.COSMOS_DB_KEY;
 
-const client = new CosmosClient({ endpoint, key });
-
-app.http('getNoodles', {
+app.http('diagnoseCosmos', {
     methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         try {
+            const client = new CosmosClient({ endpoint, key });
             const { databases } = await client.databases.readAll().fetchAll();
+
+            const results = [];
+
+            for (const db of databases) {
+                const database = client.database(db.id);
+                const { containers } = await database.containers.readAll().fetchAll();
+
+                results.push({
+                    database: db.id,
+                    containers: containers.map(c => c.id)
+                });
+            }
+
             return {
                 status: 200,
-                body: databases
+                body: results
             };
         } catch (error) {
             return {
